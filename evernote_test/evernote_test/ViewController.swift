61aee4129b829d838ff9fa92e39f3cbd347b772b
YosukeIshida <ishida.ykr@gmail.com>
2021-02-13 23:26:55 +0900

    ノートブック一覧取得成功/スワイプ成功

--- evernote_test/evernote_test/ViewController.swift
+++ evernote_test/evernote_test/ViewController.swift
@@ -8,25 +8,32 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var NoteTitle: UITextField!
     @IBOutlet weak var NoteContent: UITextView!
+    @IBOutlet var testView: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        let swipe = UISwipeGestureRecognizer(target: self, action: #selector(onSwiped(_:)))
+        swipe.direction = .left
+        testView.addGestureRecognizer(swipe)
+
+    }
+    
+    //    タイムスタンプを押す操作
+        @IBAction func onTime(_ sender: Any) {
+            timestamp(text: NoteContent)
+        }
     
     
+    
+//    ノートを投稿する操作
     @IBAction func postNote(_ sender: Any) {
         postTestNote(title: NoteTitle.text!, content: NoteContent.text!)
         NoteTitle.text = ""
         NoteContent.text = ""
     }
-    @IBAction func onTime(_ sender: Any) {
-        timestamp(text: NoteContent)
-    }
 
-//    スワイプ
-    @IBAction func swipeRight(_ sender: Any) {
-        postTestNote(title: NoteTitle.text!, content: NoteContent.text!)
-        NoteTitle.text = ""
-        NoteContent.text = ""
-    }
-    
-    
+//    タイムスタンプを押す関数
     func timestamp(text:UITextView){
         let dt = Date()
         let dateFormatter = DateFormatter()
@@ -35,6 +42,7 @@ class ViewController: UIViewController {
         text.text! += dateString
     }
     
+//    ノートを投稿する関数
     private func postTestNote(title:String,content:String) {
         ENSession.setSharedSessionConsumerKey(CONSUMER_KEY, consumerSecret: CONSUMER_SECRET,
 optionalHost: ENSessionHostSandbox)
@@ -42,13 +50,27 @@ optionalHost: ENSessionHostSandbox)
         let session = ENSession.shared
         if session.isAuthenticated {
             let note = ENNote()
-            note.title = title
-            note.content = ENNoteContent(string: content)
-            session.upload(note, notebook: nil, completion: { noteRef, error in
-                if error == nil {
-                    print("Note Upload OK")
-                } else {
-                    print("Upload note error: \(error)")
+            session.findNotes(with:nil, in: nil, orScope: .appNotebook, sortOrder: .recentlyCreated, maxResults: 10, completion: {notes,error in
+                notes?.forEach{note in
+                    print(note.title ?? "nilでした")
+                }
+            })
+            var notebook:ENNotebook?
+            session.listNotebooks(completion: {notebooks,error in
+                if error == nil{
+//                    1つ目のノートブックを取ってくる
+                notebook = notebooks?[0]
+                    print(notebook?.name ?? "nil")
+                    note.title = title
+                    note.content = ENNoteContent(string: content)
+//                    1つ目のノートブックに投稿する
+                    session.upload(note, notebook: notebook, completion: { noteRef, error in
+                        if error == nil {
+                            print("Note Upload OK")
+                        } else {
+                            print("Upload note error: \(error)")
+                        }
+                    })
                 }
             })
         } else {
@@ -62,13 +84,15 @@ optionalHost: ENSessionHostSandbox)
         }
     }
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        
-
+//    スワイプの時の操作
+    @objc func onSwiped(_ sender:UISwipeGestureRecognizer){
+        postTestNote(title: NoteTitle.text!, content: NoteContent.text!)
+        NoteTitle.text = ""
+        NoteContent.text = ""
+        print("スワイプ成功")
     }
-   
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
